a17416b14e2d884272eaf8e239cec3d7e58e6c15
YaeMonilc <Monilc@outlook.com>
2024-05-12 01:07:41 +0800

    add reflections

---
+++ src/main/kotlin/util/ReflectUtils.kt
@@ -0,0 +1,15 @@
+package ltd.bauhinia.util
+
+import org.reflections.Reflections
+
+private val reflections = Reflections("ltd.bauhinia")
+
+private fun processPackageName(packageName: String): String {
+    return packageName.replace('.', '/')
+}
+
+fun <T> forEachSubTypes(clazz: Class<out T>, block: (clazz: Class<out T>) -> Unit) {
+    reflections.getSubTypesOf(clazz).forEach {
+        block(it)
+    }
+}
