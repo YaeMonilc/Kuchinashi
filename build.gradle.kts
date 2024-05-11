d3336979731d4b3b653c2504320f5b55356bbb0c
YaeMonilc <Monilc@outlook.com>
2024-05-12 00:19:39 +0800

    add kotlin-compiler-embeddable

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,17 @@ group = "ltd.bauhinia"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-
     maven("https://maven.aliyun.com/repository/central")
     maven("https://maven.aliyun.com/repository/public")
+
+    mavenCentral()
 }
 
 dependencies {
+
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-compiler-embeddable
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.24")
+
     testImplementation(kotlin("test"))
 }
 
