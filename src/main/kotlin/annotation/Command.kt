d51cff2ed93310f8c537be7644c3ab96d5704783
YaeMonilc <Monilc@outlook.com>
2024-05-12 14:07:25 +0800

    optimize command

--- src/main/kotlin/annotation/Command.kt
+++ src/main/kotlin/annotation/Command.kt
@@ -2,9 +2,7 @@ package ltd.bauhinia.annotation
 
 annotation class Command(
     val aliases: Array<String> = [],
-    val argsName: Array<String> = [],
-    val argsType: Array<String> = [],
-    val argsRequire: BooleanArray = [],
+    val args: Array<String> = [],
     val description: String = "",
     val enable: Boolean = true,
 )
