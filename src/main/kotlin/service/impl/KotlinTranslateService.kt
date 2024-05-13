a185fcb062a34d93433a13b0931b407b1695ea86
YaeMonilc <Monilc@outlook.com>
2024-05-14 01:11:58 +0800

    add more function

--- src/main/kotlin/service/impl/KotlinTranslateService.kt
+++ src/main/kotlin/service/impl/KotlinTranslateService.kt
@@ -2,9 +2,12 @@ package ltd.bauhinia.service.impl
 
 import ltd.bauhinia.enum.KotlinTokens
 import ltd.bauhinia.service.IService
-import ltd.bauhinia.util.logger
+import ltd.bauhinia.util.forEachFiles
+import ltd.bauhinia.util.ifFalse
+import java.io.File
 
 object KotlinTranslateService : IService {
+    const val SUFFIX = ".kt"
     private val operators: List<Char> = listOf(
         ' ',
         '+',
@@ -35,8 +38,11 @@ object KotlinTranslateService : IService {
         ';'
     )
 
-    private fun translateToken(token:String): String? {
-        return KotlinTokens.getByAliasName(token)?.originName
+    private fun translateToken(token:String, reserved: Boolean = false): String? {
+        return if (!reserved)
+            KotlinTokens.getByAliasName(token)?.originName
+        else
+            KotlinTokens.getByOriginName(token)?.aliasName
     }
 
     fun translate(code: String): String {
@@ -57,5 +63,41 @@ object KotlinTranslateService : IService {
         }
 
         return translated.toString()
+    }
+
+    fun translate(code: File): String {
+        return translate(code.readText())
+    }
+
+    fun translate(code: File, to: File) {
+        to.writeText(translate(code))
+    }
+
+    fun translate(dictionaryFile: File, toDictionary: File, beforeAction: (File) -> Boolean) {
+        toDictionary.apply {
+            exists().ifFalse {
+                mkdirs()
+            }
+        }
+
+        forEachFiles(dictionaryFile) {
+            val toFile = File(toDictionary, it.path.replace(dictionaryFile.path, ""))
+
+            toFile.parentFile.apply {
+                exists().ifFalse {
+                    mkdirs()
+                }
+            }
+
+            if (beforeAction(it)) {
+                translate(it, toFile.apply {
+                    exists().ifFalse {
+                        createNewFile()
+                    }
+                })
+            } else {
+                it.copyTo(toFile)
+            }
+        }
     }
 }
