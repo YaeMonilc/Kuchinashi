fb7d8ec0a10d4efe00de5aae0592472a8a0117be
YaeMonilc <Monilc@outlook.com>
2024-05-24 21:00:35 +0800

    upgrade kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
 }
 
 group = "ltd.bauhinia"
@@ -16,7 +16,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.reflections/reflections
     implementation("org.reflections:reflections:0.10.2")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-compiler-embeddable
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.24")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.0.0")
 
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
     implementation("ch.qos.logback:logback-classic:1.5.6")
